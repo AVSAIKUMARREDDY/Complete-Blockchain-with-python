3635cd20163b235b604926835c5ef92e6d691c41
AVSAIKUMARREDDY <39004544+AVSAIKUMARREDDY@users.noreply.github.com>
2023-03-11 13:29:13 +0530

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,93 @@
+import datetime
+import json
+
+import requests
+from flask import render_template, redirect, request
+
+from app import app
+
+# The node with which our application interacts, there can be multiple
+# such nodes as well.
+CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8000"
+
+posts = []
+
+print("test : ",CONNECTED_NODE_ADDRESS)
+
+def fetch_posts():
+    """
+    Function to fetch the chain from a blockchain node, parse the
+    data and store it locally.
+    """
+    get_chain_address = "{}/chain".format(CONNECTED_NODE_ADDRESS)
+    response = requests.get(get_chain_address)
+    if response.status_code == 200:
+        content = []
+        chain = json.loads(response.content)
+        print("peers",chain["peers"])
+        for block in chain["chain"]:
+            for tx in block["transactions"]:
+                tx["index"] = block["index"]
+                tx["hash"] = block["previous_hash"]
+                content.append(tx)
+
+        global posts
+        posts = sorted(content, key=lambda k: k['timestamp'],
+                       reverse=True)
+
+
+@app.route('/')
+def index():
+    fetch_posts()
+    #print("helloo index",blockchain.chain)
+    return render_template('index.html',
+                           title='YourNet: Decentralized '
+                                 'content sharing',
+                           posts=posts,
+                           node_address=CONNECTED_NODE_ADDRESS,
+                           readable_time=timestamp_to_string)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+	print("the entered user details in the post now we will route to users in the database")
+	pk=request.form['public-key']
+	post_object={"pk":pk,}
+	print("key",pk,type(pk))
+	#submit the public key
+	new_tx_address = "{}/regfunc".format(CONNECTED_NODE_ADDRESS)
+	requests.post(new_tx_address,json=post_object,headers={'Content-type': 'application/json'})
+	return redirect('/')
+	
+	
+
+@app.route('/submit', methods=['POST'])
+def submit_textarea():
+    """
+    Endpoint to create a new transaction via our application.
+    """
+    post_content = request.form["content"]
+    author = request.form["author"]
+    signature=request.form["signature"]
+
+    print(post_content,author,signature)
+    print(type(post_content),type(author),type(signature))
+
+    post_object = {
+        'author': author,
+        'content': post_content,
+        'signature':signature,
+    }
+
+    # Submit a transaction
+    new_tx_address = "{}/new_transaction".format(CONNECTED_NODE_ADDRESS)
+
+    requests.post(new_tx_address,
+                  json=post_object,
+                  headers={'Content-type': 'application/json'})
+
+    return redirect('/')
+
+
+def timestamp_to_string(epoch_time):
+    return datetime.datetime.fromtimestamp(epoch_time).strftime('%H:%M')
